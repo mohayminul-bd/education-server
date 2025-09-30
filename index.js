333b475895820a33047106bf1a3f3b5fa3faf80c
mohayminul-bd <mohayminulislam514@gmail.com>
2025-09-30 22:54:10 +0600

    firebase token resolve

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -22,9 +20,6 @@ if (!admin.apps.length) {
 app.use(cors());
 app.use(express.json());
 
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASS:", process.env.DB_PASS);
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mljuhsj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wqymk7z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
@@ -58,16 +53,6 @@ const verifyTokenEmail = async (req, res, next) => {
   }
   next();
 };
-
-// //JWT TOKEN RELETEDE API
-app.post("/jwt", async (req, res) => {
-  const { email } = req.body;
-  const user = { email };
-  const token = JsonWebTokenError.sign(user, process.env.JWT_ACCESS_SECRET, {
-    expressIn: "1h",
-  });
-  res.send({ token });
-});
 
 async function run() {
   try {
@@ -428,7 +413,7 @@ async function run() {
         }
       }
     );
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("✅ Successfully connected to MongoDB!");
   } catch (error) {
     console.error("❌ Error Connecting to MongoDB:", error);
